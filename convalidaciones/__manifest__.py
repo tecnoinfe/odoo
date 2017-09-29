3b32d6c64d532d4e353e652ea493316fc778b190
Victor Hugo <victorzevallos@protonmail.com>
2017-09-28 19:28:41 -0500

    Primer commit

---
+++ convalidaciones/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "convalidaciones",
+
+    'summary': """
+        Permite gestionar las convalidaciones en un Instituto""",
+
+    'description': """
+        Este módulo permite gestionar las convalidaciones de un centro educativo
+    """,
+
+    'author': "My Company",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
