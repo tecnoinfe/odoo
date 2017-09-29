8293fabb50c6ab93cbfad77214bf654fde3ce61f
Victor Hugo <victorzevallos@protonmail.com>
2017-09-28 23:35:01 -0500

    App Convalidaciones

--- convalidaciones/__manifest__.py
+++ convalidaciones/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "convalidaciones",
+    'name': "Convalidaciones",
 
     'summary': """
         Permite gestionar las convalidaciones en un Instituto""",
@@ -9,26 +9,18 @@
         Este módulo permite gestionar las convalidaciones de un centro educativo
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Victor",
+    'website': "http://www.example.com",
 
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/master/odoo/addons/base/module/module_data.xml
-    # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
 
-    # any module necessary for this one to work correctly
     'depends': ['base'],
 
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
-    ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+        'views/alumnos.xml',
+        'views/modulos.xml',
+        'views/ciclos.xml',
+        'views/convalidaciones.xml',
     ],
 }
--- convalidaciones/models/models.py
+++ convalidaciones/models/models.py
@@ -2,14 +2,34 @@
 
 from odoo import models, fields, api
 
-# class convalidaciones(models.Model):
-#     _name = 'convalidaciones.convalidaciones'
+class Alumno(models.Model):
+    _name = 'convalidaciones.alumno'
 
-#     name = fields.Char()
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
-#     description = fields.Text()
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         self.value2 = float(self.value) / 100
+    name = fields.Char(string='Nombre y apellidos')
+    edad = fields.Integer(string='Edad')
+    localidad = fields.Char(string='Localidad')
+    provincia = fields.Char(string='Provincia')
+    email = fields.Char(string='Email')
+    convalidacion_ids = fields.One2many('convalidaciones.convalidacion', 'alumno_id', string='Convalidaciones del alumno')
+
+class Modulo(models.Model):
+    _name = 'convalidaciones.modulo'
+
+    name = fields.Char(string='Nombre')
+    description = fields.Text(string='Descripción')
+    ciclo_ids = fields.Many2many('convalidaciones.ciclo')
+
+class Convalidacion(models.Model):
+    _name = 'convalidaciones.convalidacion'
+
+    name = fields.Char()
+    fecha_convalidacion = fields.Date(string='Fecha de la convalidación')
+    modulo_id = fields.Many2one('convalidaciones.modulo', string='Módulo')
+    alumno_id = fields.Many2one('convalidaciones.alumno', string='Alumno')
+
+class Ciclos(models.Model):
+    _name = 'convalidaciones.ciclo'
+
+    name = fields.Char(string='Nombre')
+    description = fields.Text(string='Descripción')
+    modulo_ids = fields.Many2many('convalidaciones.modulo')
